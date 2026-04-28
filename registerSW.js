efb5fb189a887980aa2f6cfcf07a2694eb702e74
vishvchavan07 <vishvcode@gmail.com>
2026-04-28 14:48:11 +0000

    Deploying to gh-pages from @ vishvchavan07/AashaLink_v2.3@f221c61e6df70cab392e8e6465292081fa522e37 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AashaLink_v2.3/sw.js', { scope: '/AashaLink_v2.3/' })})}
